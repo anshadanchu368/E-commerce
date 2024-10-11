ea943ac1438fe1f20dfc2032407925161c8f68b2
Hashim Anhad <anshadanchu368@gmail.com>
2024-10-11 19:47:13 +0530

    product models setup

---
+++ src/models/productModels.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema({
+    name: {type:String, required:true },
+    description: {type:String, required:true },
+    price : {type: Number, required:true},
+    category: {
+       type:mongoose.Schema.Types.ObjectId,
+       ref:"categoryName",
+       required:true
+    },
+    images: [{ type:String, required: true}],
+    stock: {type:Number, default:0, min:0},
+    deleted: {type:Boolean, default:false},
+    offer: {
+        amount: {type:Number, default: 0},
+        isActive: {type:Boolean, default: false}
+    }
+})
+
+const Product = mongoose.model("Product", productSchema)
+
+module.exports = Product;
