616d4d1fa8ddc2b29faa6ef0e3ac73ed03a0064f
Hashim Anhad <anshadanchu368@gmail.com>
2024-10-14 20:24:55 +0530

    logout method done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -101,5 +101,30 @@ const loginUser = asyncHandler(async (req,res)=>{
         )
     )
 
-
 })
+
+const logoutUser = asyncHandler(async(req,res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset:{
+                refreshToken:1
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+        .status(200)
+        .clearCookie("accessToken",options)
+        .clearCookie("refreshToken",options)
+        .json(new ApiResponse(200, null, "User logged out successfully"))
+})
+
